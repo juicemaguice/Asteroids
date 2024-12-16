b46c4abb167d7c78e781ea9d1bc89d725259a4cb
juicemaguice <11728171+juicemaguice@users.noreply.github.com>
2024-12-16 04:44:41 -0600

    limited to 60fps

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock, dt = pygame.time.Clock(), 0
 
     while True:
         for event in pygame.event.get():
@@ -11,6 +12,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
 
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
     # ^ keep display.flip() as final call before loop
 
