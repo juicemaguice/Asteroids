e79dfa912ee1e4cb9b71e00af87274b33edd7451
juicemaguice <11728171+juicemaguice@users.noreply.github.com>
2024-12-16 10:58:21 -0600

    added asteroid object and asteroidfield spawner

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-#import pygame
+import pygame
 from constants import *
-from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from player import Player
 
 def main():
     pygame.init()
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock, dt = pygame.time.Clock(), 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
-        for obj in updatable:
+        for obj in updatables:
             obj.update(dt)
-        for obj in drawable:
+        for obj in drawables:
             obj.draw(screen)
 
         pygame.display.flip()
