9a76386a18666e23c9d0fc5e3d034d6b2ee94dd1
juicemaguice <11728171+juicemaguice@users.noreply.github.com>
2024-12-16 04:38:02 -0600

    game window opens

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+
+        pygame.display.flip()
+    # ^ keep display.flip() as final call before loop
+
+
+if __name__ == "__main__":
+    main()
